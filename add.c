1f1558fbebe5bec9c3849ecee693b69ee4610ba1
vados <vadimvase@gmail.com>
2025-03-09 15:03:22 +0600

    obnova

---
+++ add.c
@@ -0,0 +1,11 @@
+#include "add.h"
+
+int add_int(int a, int b) { return a + b; }
+int max(int a, int b){
+    if (a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+};
---
+++ add.h
@@ -0,0 +1,2 @@
+int add_int(int a, int b);
+int max(int a, int b);
